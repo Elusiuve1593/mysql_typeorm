efdd0ab302c6afa6483148f87c6f1b603c085819
Roma <elusiuve15.09.93@icloud.com>
2024-06-05 18:34:21 +0300

    commit

--- src/config/database.module.ts
+++ src/config/database.module.ts
@@ -1,21 +1,18 @@
 import { Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Post } from 'src/user/entities/Post';
-import { User } from 'src/user/entities/User';
 
 @Module({
   imports: [
     TypeOrmModule.forRootAsync({
       useFactory: (configService: ConfigService) => ({
         type: 'mysql',
-        host: configService.getOrThrow('MYSQL_HOST'),
-        port: configService.getOrThrow('MYSQL_PORT'),
-        database: configService.getOrThrow('MYSQL_DATABASE'),
-        username: configService.getOrThrow('MYSQL_USERNAME'),
-        synchronize: configService.getOrThrow('MYSQL_SYNC'),
-        autoLoadEntities: true,
-        entities: [User, Post],
+        host: configService.getOrThrow('TYPEORM_HOST'),
+        port: configService.getOrThrow('TYPEORM_PORT'),
+        database: configService.getOrThrow('TYPEORM_DATABASE'),
+        username: configService.getOrThrow('TYPEORM_USERNAME'),
+        synchronize: configService.getOrThrow('TYPEORM_SYNC'),
+        autoLoadEntities: configService.getOrThrow('TYPEORM_AUTO')
       }),
       inject: [ConfigService],
     }),
